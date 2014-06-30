d0c3c3f912e1d17f2261ca7fa82b5a87bb827deb
helenswanson <hhyswanson@gmail.com>
2014-06-30 01:07:10 -0400

    Created spec for user adding a new car.

--- spec/features/user_adds_a_new_car_spec.rb
+++ spec/features/user_adds_a_new_car_spec.rb
@@ -14,11 +14,50 @@ feature 'user adds a new car', %Q(
 # • If I do not specify all of the required information in the required formats, the car is not recorded and I am presented with errors.
 # • Upon successfully creating a car, I am redirected back to the index of cars.
 
-  scenario 'user successfully adds a new car' do
-
+  before :each do
+    visit new_car_path
   end
 
-  scenario
+  context 'user completes all required fields' do
+    before :each do
+      visit new_car_path
+      car = FactoryGirl.create(:car)
 
+      fill_in 'Type', with: car.type
+      fill_in 'Manufacturer', with: car.manufacturer
+      fill_in 'Color', with: car.color
+      fill_in 'Year', with: car.year
+      fill_in 'Mileage', with: car.mileage
+    end
 
+    scenario 'user adds a new car' do
+      fill_in 'Year', with: car.year
+      click_on 'Create Car'
 
+      expect(page).to have_content 'Success'
+      expect(page).to have_content car.type
+      expect(page).to have_content car.manufacturer
+      expect(page).to have_content car.color
+      expect(page).to have_content car.year
+      expect(page).to have_content car.mileage
+    end
+
+    scenario 'user enters invalid car year' do
+      fill_in 'Year', with: 1919
+
+      expect(page).to_not have_content car.type
+      expect(page).to_not have_content car.manufacturer
+      expect(page).to_not have_content car.color
+      expect(page).to have_content "can't be blank"
+      expect(page).to_not have_content car.mileage
+    end
+  end
+
+  scenario 'user enters incomplete car info' do
+    visit new_car_path
+    click_on 'Create Car'
+
+    expect(page).to_not have_content 'Success'
+    expect(page).to have_content "can't be blank"
+  end
+end
