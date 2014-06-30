e251634f52a86d33ce0a1bc06718e5242a062ff2
helenswanson <hhyswanson@gmail.com>
2014-06-30 01:17:03 -0400

    Refactored spec for user adding a new car

--- spec/features/user_adds_a_new_car_spec.rb
+++ spec/features/user_adds_a_new_car_spec.rb
@@ -14,10 +14,6 @@ feature 'user adds a new car', %Q(
 # • If I do not specify all of the required information in the required formats, the car is not recorded and I am presented with errors.
 # • Upon successfully creating a car, I am redirected back to the index of cars.
 
-  before :each do
-    visit new_car_path
-  end
-
   context 'user completes all required fields' do
     before :each do
       visit new_car_path
@@ -26,11 +22,10 @@ feature 'user adds a new car', %Q(
       fill_in 'Type', with: car.type
       fill_in 'Manufacturer', with: car.manufacturer
       fill_in 'Color', with: car.color
-      fill_in 'Year', with: car.year
       fill_in 'Mileage', with: car.mileage
     end
 
-    scenario 'user adds a new car' do
+    scenario 'user adds valid car year' do
       fill_in 'Year', with: car.year
       click_on 'Create Car'
 
@@ -45,11 +40,8 @@ feature 'user adds a new car', %Q(
     scenario 'user enters invalid car year' do
       fill_in 'Year', with: 1919
 
-      expect(page).to_not have_content car.type
-      expect(page).to_not have_content car.manufacturer
-      expect(page).to_not have_content car.color
+      expect(page).to_not have_content 'Success'
       expect(page).to have_content "can't be blank"
-      expect(page).to_not have_content car.mileage
     end
   end
 
