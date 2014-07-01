dd280d6485f0c8e0b2527c3170fbf27a5f4f1b3f
helenswanson <hhyswanson@gmail.com>
2014-06-30 21:21:31 -0400

    Migrated creating manufacturers and creating cars.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630185337) do
+ActiveRecord::Schema.define(version: 20140630213314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20140630185337) do
   end
 
   create_table "manufacturers", force: true do |t|
+    t.string   "model",      null: false
+    t.string   "country",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
